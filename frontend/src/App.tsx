a4d3c25c46c59cf9f6c738e1f946c3f0bc808a17
joung <15gpfk@naver.com>
2023-09-13 12:36:25 +0900

    Feat: Setup project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,23 @@ import { Route, Routes } from 'react-router-dom';
 // 각 페이지 정보
 import LoginPage from './pages/loginPage';
 import MainPage from './pages/mainPage';
+import TradeMainPage from './pages/tradeMainPage';
+import CommunityPage from './pages/communityPage';
+import AuctionPage from './pages/auctionPage';
 
 const App = () => {
   return (
     <Routes>
       {/* 로그인 페이지 */}
-      <Route path='/' element={<LoginPage />} />
+      <Route path='/login' element={<LoginPage />} />
       {/* 메인 페이지 */}
-      <Route path='/main' element={<MainPage />} />
+      <Route path='/' element={<MainPage />} />
+      {/* 중고거래 메인 페이지 */}
+      <Route path='/tradeMain' element={<TradeMainPage />} />
+      {/* 경매 페이지 */}
+      <Route path='/auction' element={<AuctionPage />} />
+      {/* 커뮤티니 페이지 */}
+      <Route path='/community' element={<CommunityPage />} />
     </Routes>
   );
 };
