77eb92635378131b2101074f6c609421e1b1f202
joung <15gpfk@naver.com>
2023-09-14 17:54:12 +0900

    Feat: mainpage

--- frontend/src/components/mainPage.styles.tsx
+++ frontend/src/components/mainPage.styles.tsx
@@ -1,5 +1,5 @@
 import { styled } from 'styled-components';
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const MainPage = styled.div`
   width: 100%;
@@ -7,125 +7,153 @@ const MainPage = styled.div`
 `;
 
 const Page = styled.div`
-  overflow-x: hidden;
   display: flex;
+  overflow: hidden;
+  /* scroll-behavior: smooth; */
 `;
-const PagesSection = styled.section`
-  min-width: 100vw;
+const PagesSection1 = styled.section`
+  min-width: 130vw;
   min-height: 100vh;
   display: flex;
-  justify-content: center;
+  /* justify-content: center; */
   align-items: center;
   font-size: 4ch;
-  border: 1px solid black;
+  /* overflow: hidden; */
+  /* border: 1px solid black; */
+`;
+const PagesSection2 = styled.section`
+  min-width: 1000vw;
+  min-height: 100vh;
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  font-size: 4ch;
+  /* overflow: hidden; */
+  /* border: 1px solid black; */
 `;
 const Pages = styled.div`
   /* position: relative;
     width: 100vw;
     display: flex; */
+  /* overflow: hidden; */
 `;
 const Pages1 = styled.div`
   display: flex;
+  align-items: center;
+  /* overflow: hidden; */
+  margin-left: 100px;
 `;
 const Pages1_Left = styled.div`
   position: relative;
-  width: 40vw;
-  height: 80vh;
+  width: 530px;
+  height: 500px;
   /* border: 1px solid black; */
-  margin-right: 10vw;
+  margin-right: 200px;
   /* align-items: center; */
 `;
 
 const Pages1_tag1 = styled.p`
-  font-size: 3vw;
-  margin-top: 37vh;
+  font-size: 35px;
+  margin-top: 240px;
 `;
 const Pages1_tag2 = styled.p`
-  font-size: 2.5;
+  font-size: 15px;
 `;
 const Pages1_Img1 = styled.img`
-  width: 25vw;
-  height: 40vh;
+  width: 320px;
+  height: 200px;
   position: absolute;
-  left: 14vw;
-  top: 2vh;
+  left: 280px;
+  top: 300px;
+  border-radius: 20px;
   animation: moveUpDown 1s infinite alternate; /* 애니메이션 적용 */
   @keyframes moveUpDown {
     0% {
-      top: 2vh;
+      top: 50px;
     }
     100% {
-      top: 3vh; /* 1초에 2vh 이동 */
+      top: 59px; /* 1초에 2vh 이동 */
     }
   }
 `;
 
 const Pages1_Right = styled.div`
+  /* position: absolute;
+  top: 110px;
+  left: 700px; */
   position: relative;
-  width: 35vw;
-  height: 80vh;
-  border: 1px solid black;
+  /* width: 480px;
+  height: 540px; */
+  /* border: 1px solid black; */
+  /* transform: rotate(20deg); */
+  overflow: hidden;
+  width: 550px;
+  height: 550px;
+  /* width: 320px;
+  height: 320px; */
+  @media (max-width: 1000px) {
+    width: 420px;
+    height: 420px;
+  }
+
+  transform: rotate(0deg); /* 초기 회전 각도 */
+  transition: transform 0.2s ease;
 `;
 
-const Pages1_box1 = styled.div`
+const Pages1_img1 = styled.img`
   position: absolute;
-  top: 42vh;
-  left: 0vw;
-  width: 18vw;
-  height: 8vh;
-  clip-path: polygon(0% 0%, 80% 0%, 100% 100%, 20% 100%);
-  background: red;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  width: 550px;
+  height: 550px;
+  /* width: 320px;
+  height: 320px; */
+  /* border: 20px solid red; */
+  @media (max-width: 1000px) {
+    width: 420px;
+    height: 420px;
+  }
 `;
-const Pages1_box2 = styled.div`
-  position: absolute;
-  top: 55vh;
-  left: 5vw;
-  width: 18vw;
-  height: 8vh;
 
-  clip-path: polygon(0% 0%, 80% 0%, 100% 100%, 20% 100%);
-  background: red;
+const Pages2 = styled.div`
+  display: flex;
+  /* align-items: center; */
+  /* overflow: hidden; */
+  margin-left: 10px;
 `;
-const Pages1_box3 = styled.div`
-  position: absolute;
-  top: 20vh;
-  left: 0vw;
-  width: 18vw;
-  height: 10vh;
-  clip-path: polygon(0% 0%, 80% 0%, 100% 100%, 20% 100%);
-  transform: rotate(120deg);
 
-  background: blue;
+const Page2_tag1 = styled.div`
+  margin-top: 330px;
+  margin-right: 150px;
+  font-size: 40px;
+  width: 800px;
+  height: 200px;
+  border: 2px solid red;
 `;
-const Pages1_box4 = styled.div`
-  width: 19vw;
-  height: 10vh;
-  clip-path: polygon(0% 0%, 80% 0%, 100% 100%, 20% 100%);
-  transform: rotate(120deg);
 
-  background: blue;
+const Page2_tag2 = styled.div`
+  margin-top: 100px;
+  font-size: 40px;
+  width: 400px;
+  height: 400px;
+  border: 2px solid red;
+  margin-right: 300px;
 `;
-const Pages1_box5 = styled.div`
-  position: absolute;
-  top: 36vh;
-  left: 15vw;
-  width: 18vw;
-  height: 10vh;
-  clip-path: polygon(0% 0%, 80% 0%, 100% 100%, 20% 100%);
-  transform: rotate(240deg);
-
-  background: yellow;
-`;
-const Pages1_box6 = styled.div`
-  width: 19vw;
-  height: 10vh;
-  clip-path: polygon(0% 0%, 80% 0%, 100% 100%, 20% 100%);
-  transform: rotate(240deg);
-
-  background: yellow;
+const Page2_colorbox = styled.div`
+  margin-top: -100px;
+  font-size: 40px;
+  width: 4000px;
+  height: 100vh;
+  border: 2px solid red;
+  clip-path: polygon(20% 0%, 100% 0%, 100% 100%, 0% 100%);
+  background: #ff0000;
 `;
 
 const PageSlide = () => {
+  const [rotation, setRotation] = useState(0);
+  const [limsize, setLimSize] = useState({ width: 450, height: 450 });
+  const [size, setSize] = useState({ width: 450, height: 450 });
   useEffect(() => {
     // 컴포넌트가 마운트될 때 실행되는 코드
     const scrollContainer: HTMLElement | null =
@@ -135,6 +163,49 @@ const PageSlide = () => {
       evt.preventDefault();
       if (scrollContainer) {
         scrollContainer.scrollLeft += evt.deltaY;
+        console.log(evt.deltaY, scrollContainer.scrollLeft);
+      }
+      if (evt.deltaY < 0 && scrollContainer.scrollLeft == 0) {
+        setRotation(0);
+        setSize((prevSize) => ({
+          width: 450, // 가로 크기 5px 증가
+          height: 450 // 세로 크기 5px 증가
+        }));
+        setLimSize((prevSize) => ({
+          width: 450, // 가로 크기 5px 증가
+          height: 450 // 세로 크기 5px 증가
+        }));
+      }
+      if (scrollContainer.scrollLeft > 0 && scrollContainer.scrollLeft < 1250) {
+        if (evt.deltaY > 0) {
+          // 이미지 반시계 회전
+          setRotation((prevRotation) => prevRotation - 13);
+          //이미지 크기 변경
+          setSize((prevSize) => ({
+            width: prevSize.width + 35, // 가로 크기 5px 증가
+            height: prevSize.height + 35 // 세로 크기 5px 증가
+          }));
+          if (scrollContainer.scrollLeft < 1050) {
+            setLimSize((prevSize) => ({
+              width: prevSize.width + 55, // 가로 크기 5px 증가
+              height: prevSize.height + 35 // 세로 크기 5px 증가
+            }));
+          }
+        } else {
+          // (복구) 이미지 시계 회전
+          setRotation((prevRotation) => prevRotation + 13);
+          //이미지 크기 변경
+          setSize((prevSize) => ({
+            width: prevSize.width - 35, // 가로 크기 5px 증가
+            height: prevSize.height - 35 // 세로 크기 5px 증가
+          }));
+          if (scrollContainer.scrollLeft < 1050) {
+            setLimSize((prevSize) => ({
+              width: prevSize.width - 55, // 가로 크기 5px 증가
+              height: prevSize.height - 35 // 세로 크기 5px 증가
+            }));
+          }
+        }
       }
     };
 
@@ -149,35 +220,54 @@ const PageSlide = () => {
       }
     };
   }, []);
+
   return (
     <Page className='page-container'>
-      <PagesSection>
+      <PagesSection1>
         <Pages1>
           <Pages1_Left>
             <Pages1_tag1>
               <Pages1_Img1 src='/assets/img/mainicon1.PNG'></Pages1_Img1>
-              대충 <br />
-              아무내용{' '}
-              <span style={{ color: '#F47B55' }}>거래,경매,게시판</span>
+              빠르고 간단하게 <br />
+              상품을 팔거나 살 수 있는
+              <span style={{ color: '#F47B55' }}>스마트한 커뮤니티 장터</span>
             </Pages1_tag1>
             <Pages1_tag2>
-              ~~~~~~~~~~~~~~~ <br />
-              ~~~~~~~~~~~ <br />
-              ~~~~~
+              당신 근처의 지역 생활 커뮤니티. <br />
+              동네라서 가능한 모든 것 싸그리에서 가까운 이웃과 함께하세요.{' '}
+              <br />
             </Pages1_tag2>
           </Pages1_Left>
-          <Pages1_Right>
-            <Pages1_box1></Pages1_box1>
-            <Pages1_box2></Pages1_box2>
-            <Pages1_box3></Pages1_box3>
-            <Pages1_box5></Pages1_box5>
+          <Pages1_Right
+            style={{
+              width: `${limsize.width}px`,
+              height: `${limsize.height}px`
+            }}
+          >
+            <Pages1_img1
+              src='/assets/img/mainbox1.png'
+              alt=''
+              style={{
+                transform: `rotate(${rotation}deg)`,
+                width: `${size.width}px`,
+                height: `${size.height}px`
+              }}
+            />
           </Pages1_Right>
         </Pages1>
-      </PagesSection>
-      <PagesSection>
-        <Pages>2</Pages>
-      </PagesSection>
-      <PagesSection>
+      </PagesSection1>
+      <PagesSection2>
+        <Pages2>
+          <Page2_tag1>
+            당신 근처의 지역 생활 커뮤니티. <br />
+            동네라서 가능한 모든 것 싸그리에서 가까운 이웃과 함께하세요.
+            <br />
+          </Page2_tag1>
+          <Page2_tag2>이미지</Page2_tag2>
+          <Page2_colorbox></Page2_colorbox>
+        </Pages2>
+      </PagesSection2>
+      {/* <PagesSection>
         <Pages>3</Pages>
       </PagesSection>
       <PagesSection>
@@ -191,7 +281,7 @@ const PageSlide = () => {
       </PagesSection>
       <PagesSection>
         <Pages>7</Pages>
-      </PagesSection>
+      </PagesSection> */}
     </Page>
   );
 };
