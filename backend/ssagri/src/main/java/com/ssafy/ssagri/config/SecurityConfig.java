a044857447480e693cbed30290eeb5ca7d8612fe
신창학 <csg1353@naver.com>
2023-09-15 17:28:56 +0900

    Feat : Swagger API 추가, Filter 및 Security에 인가 처리 설정

--- backend/ssagri/src/main/java/com/ssafy/ssagri/config/SecurityConfig.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/config/SecurityConfig.java
@@ -24,15 +24,19 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 // common settings
-                .httpBasic().disable()
-                .csrf().disable()   //CSRF 토큰을 사용하지 않도록 설정
-                .cors()             //CORS 활성화
+                    .httpBasic().disable()
+                    .csrf().disable()   //CSRF 토큰을 사용하지 않도록 설정
+                    .cors()             //CORS 활성화
                 .and()
-                .sessionManagement()//세션 관리 설정
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션 생성 X, 요청 상태 추적하지 않음
+                    .sessionManagement()//세션 관리 설정
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션 생성 X, 요청 상태 추적하지 않음
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/webjars/**") // Swagger 경로
+                    .permitAll() // 해당 경로에 대한 접근을 모두 허용
                 .and()
         //jwt Filter Setting
-                .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class)
         ;
 
         return http.build();
