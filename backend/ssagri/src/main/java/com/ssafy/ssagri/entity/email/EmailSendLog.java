323f7212c2d03c4d9293a0fc00b5a4ad7510fb5d
신창학 <csg1353@naver.com>
2023-09-18 17:56:15 +0900

    feat : Exception Handler(AOP) 추가 및 변경, Mail 기능 추가 및 User 도메인 메서드 추가

---
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/entity/email/EmailSendLog.java
@@ -0,0 +1,40 @@
+package com.ssafy.ssagri.entity.email;
+
+
+import com.ssafy.ssagri.entity.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+public class EmailSendLog {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long emailSendLogNo;
+    @ManyToOne
+    @JoinColumn(name = "userNo", nullable = false)
+    private User user;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String verificationCode;
+    @Column(nullable = false)
+    private LocalDateTime createTime;
+    @Column(nullable = false)
+    private LocalDateTime expiryTime;
+    @Column(nullable = false)
+    private boolean used;
+
+    public EmailSendLog() {
+
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+}
