4f4e5882b033cf51ffd59177d785438cb91031b1
유태영 <tybam@naver.com>
2023-09-19 21:56:56 +0900

    Feat: Message controller, service, repository

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/message/service/MessageService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/message/service/MessageService.java
@@ -1,5 +1,7 @@
 package com.ssafy.ssagri.domain.message.service;
 
+import com.ssafy.ssagri.domain.message.dto.MessageRequest;
+import com.ssafy.ssagri.domain.message.dto.MessageResponse;
 import com.ssafy.ssagri.domain.message.repository.MessageRepository;
 import com.ssafy.ssagri.entity.chat.Message;
 import lombok.RequiredArgsConstructor;
@@ -14,15 +16,21 @@ import java.time.LocalDateTime;
 public class MessageService {
     private final MessageRepository messageRepository;
 
+    /*
+    * MessageRequest를 받아서
+    * Entity로 바꾸고 DB 저장
+    * MessageResponse로 바꿔서 client로 반환
+    * */
     @Transactional
-    public String saveMessage(){
-        Message build = Message.builder()
-                .roomId(Long.parseLong("1"))
-                .userId(Long.parseLong("1"))
-                .content("1")
-                .time(LocalDateTime.now())
-                .build();
-        messageRepository.save(build);
-        return build.getId();
+    public MessageResponse saveMessage(MessageRequest messageRequest){
+        System.out.println("messageRequest123 = " + messageRequest);
+        //Entity로 바꾸고
+        Message message = messageRequest.toEntity();
+
+        //저장
+        messageRepository.save(message);
+
+        //client로 반환
+        return messageRequest.toResponse();
     }
 }
