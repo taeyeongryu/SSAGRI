c4491c6fe51d3b070d7d5dd6cbcbd4a725e84577
유태영 <tybam@naver.com>
2023-09-20 14:55:03 +0900

    Feat: chatting

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/message/controller/MessageController.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/message/controller/MessageController.java
@@ -5,12 +5,18 @@ import com.ssafy.ssagri.domain.message.dto.MessageRequest;
 import com.ssafy.ssagri.domain.message.dto.MessageResponse;
 import com.ssafy.ssagri.domain.message.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("message")
 public class MessageController {
     private final MessageService messageService;
 
@@ -21,5 +27,10 @@ public class MessageController {
         System.out.println("messageRequest = " + messageRequest);
         MessageResponse messageResponse = messageService.saveMessage(messageRequest);
         return messageResponse;
+    }
+
+    @GetMapping("")
+    public Page<MessageResponse> selectMessage(Long roomNo, Pageable pageable){
+        return messageService.selectMessageResponse(roomNo, pageable);
     }
 }
--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/message/service/MessageService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/message/service/MessageService.java
@@ -5,16 +5,32 @@ import com.ssafy.ssagri.domain.message.dto.MessageResponse;
 import com.ssafy.ssagri.domain.message.repository.MessageRepository;
 import com.ssafy.ssagri.entity.chat.Message;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MessageService {
     private final MessageRepository messageRepository;
+
+    public Page<MessageResponse> selectMessageResponse(Long roomNo, Pageable pageable){
+        Page<Message> messages = messageRepository.findMessagesByRoomIdOrderByTimeDesc(roomNo, pageable);
+        List<MessageResponse> messageResponseList = new ArrayList<>();
+        for (Message message : messages) {
+            messageResponseList.add(message.toResponse());
+        }
+        return new PageImpl<>(messageResponseList, messages.getPageable(), messages.getTotalElements());
+    }
+
+
 
     /*
     * MessageRequest를 받아서
@@ -23,7 +39,6 @@ public class MessageService {
     * */
     @Transactional
     public MessageResponse saveMessage(MessageRequest messageRequest){
-        System.out.println("messageRequest123 = " + messageRequest);
         //Entity로 바꾸고
         Message message = messageRequest.toEntity();
 
