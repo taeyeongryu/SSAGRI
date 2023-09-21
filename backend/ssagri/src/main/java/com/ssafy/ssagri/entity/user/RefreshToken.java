b4ca9b18d7788ff54be5fec902df77d841e4c0db
신창학 <csg1353@naver.com>
2023-09-21 11:04:22 +0900

    feat : Redis 해시값 저장 삭제수정 및 CORS 변경

--- backend/ssagri/src/main/java/com/ssafy/ssagri/entity/user/RefreshToken.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/entity/user/RefreshToken.java
@@ -1,23 +1,29 @@
 package com.ssafy.ssagri.entity.user;
 
+import lombok.Data;
+import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 
 /**
  * Redis Entity
  */
 @RedisHash("RefreshToken")
-@Setter
-
+@Getter
 public class RefreshToken {
     @Id
     private Long userNo;
 
     private String refreshToken;
 
-    public RefreshToken(Long userNo, String refreshToken) {
+    @TimeToLive
+    private Long expiration; //TTL Field
+
+    public RefreshToken(Long userNo, String refreshToken, Long expiration) {
         this.userNo = userNo;
         this.refreshToken = refreshToken;
+        this.expiration = expiration;
     }
 }
