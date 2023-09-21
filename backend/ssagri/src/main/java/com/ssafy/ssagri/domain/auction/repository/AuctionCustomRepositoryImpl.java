3c7c421076fbccb6e44b98e91fd1deb3ac05544c
유혜빈 <handal@kakao.com>
2023-09-21 12:52:54 +0900

    Update AuctionCustomRepositoryImpl.java

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/repository/AuctionCustomRepositoryImpl.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/repository/AuctionCustomRepositoryImpl.java
@@ -1,10 +1,8 @@
 package com.ssafy.ssagri.domain.auction.repository;
 
-import com.querydsl.core.Tuple;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import com.ssafy.ssagri.entity.auction.AuctionProduct;
-import com.ssafy.ssagri.entity.auction.QAuctionProduct;
-import com.ssafy.ssagri.entity.auction.QAuctionProductPhoto;
+
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -15,18 +13,11 @@ public class AuctionCustomRepositoryImpl implements AuctionCustomRepository{
     private final JPAQueryFactory jpaQueryFactory;
 
 
-    public AuctionCustomRepositoryImpl(EntityManager entityManager) {
-        this.jpaQueryFactory = new JPAQueryFactory(entityManager);
-    }
 
-    // 경매 상품
-    QAuctionProduct qAuctionProduct = QAuctionProduct.auctionProduct;
-    // 경매 상품에 대한 이미지
-    QAuctionProductPhoto qAuctionProductPhoto = QAuctionProductPhoto.auctionProductPhoto;
-
+    
     // 모든 경매리스트 출력 (상품정보)
-    @Override
-    public void getAuctionProducts(){
+   // @Override
+   // public void getAuctionProducts(){
 
 
 //        jpaQueryFactory
@@ -37,7 +28,7 @@ public class AuctionCustomRepositoryImpl implements AuctionCustomRepository{
 //                .fetch();
 
 
-    }
+   // }
 
 
 
