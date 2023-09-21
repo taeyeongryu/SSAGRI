6fb124e4f83840b7d3ff3fd414712075c54e00b2
유혜빈 <handal@kakao.com>
2023-09-21 13:01:16 +0900

    Update AuctionCustomRepositoryImpl.java

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/repository/AuctionCustomRepositoryImpl.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/auction/repository/AuctionCustomRepositoryImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class AuctionCustomRepositoryImpl implements AuctionCustomRepository{
 
-    private final JPAQueryFactory jpaQueryFactory;
+    //private final JPAQueryFactory jpaQueryFactory;
 
 
 
