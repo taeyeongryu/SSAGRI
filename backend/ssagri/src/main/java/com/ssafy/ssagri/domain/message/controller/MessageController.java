c550ae629add5450866eafe6d22bd2d3cdeca476
유태영 <tybam@naver.com>
2023-09-22 11:02:34 +0900

    Update: check select, insert

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/message/controller/MessageController.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/message/controller/MessageController.java
@@ -6,19 +6,18 @@ import com.ssafy.ssagri.domain.message.dto.MessageResponseDto;
 import com.ssafy.ssagri.domain.message.service.MessageService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("message")
+@Slf4j
 public class MessageController {
     private final MessageService messageService;
 
@@ -26,20 +25,24 @@ public class MessageController {
     @MessageMapping("/chat/room/1")
     @SendTo("/sub/chat/room/1")
     @ApiOperation("메시지 보내는 메서드")
-    public MessageResponseDto sendMessage(MessageRequestDto messageRequest) {
+    public MessageResponseDto sendMessage(@RequestBody MessageRequestDto messageRequest) {
         System.out.println("messageRequest = " + messageRequest);
         MessageResponseDto messageResponse = messageService.saveMessage(messageRequest);
         return messageResponse;
     }
 
-    @GetMapping("")
+    @GetMapping("/{roomNo}")
     @ApiOperation("chatRoomNo로 메시지 조회하는 메서드")
-    public Page<MessageResponseDto> selectMessage(Long roomNo, Pageable pageable){
+    public Page<MessageResponseDto> selectMessage(@PathVariable(name = "roomNo") Long roomNo, Pageable pageable){
+        log.info("roomNo = {}", roomNo);
+        log.info("pageable = {}", pageable);
         return messageService.selectMessageResponse(roomNo, pageable);
     }
 
     @PostMapping("")
-    public MessageResponseDto saveMessage(MessageRequestDto messageRequest) {
-        return messageService.saveMessage(messageRequest);
+    public MessageResponseDto saveMessage(@RequestBody MessageRequestDto messageRequest) {
+        log.info("messageRequest = {}", messageRequest);
+        MessageResponseDto messageResponseDto = messageService.saveMessage(messageRequest);
+        return messageResponseDto;
     }
 }
