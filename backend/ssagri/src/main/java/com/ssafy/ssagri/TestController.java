4af28bf49ff590acdeb6d7250ff01affbe3d37bd
신창학 <csg1353@naver.com>
2023-09-22 13:26:11 +0900

    feat : 메일 동작 확인 중간 저장

--- backend/ssagri/src/main/java/com/ssafy/ssagri/TestController.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/TestController.java
@@ -1,15 +1,50 @@
 package com.ssafy.ssagri;
 
+import com.ssafy.ssagri.util.mail.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("test")
+@RequiredArgsConstructor
 @RestController
 public class TestController {
+
+    private final JavaMailSender emailSender;
+    private final EmailService emailService;
+
+
     @GetMapping
     public String test() {
         System.out.println("TEST");
         return "TEST";
+    }
+
+
+//    @GetMapping("mail")
+//    public String test2() {
+//        try {
+//            sendEmail("syhfqq1810@gmail.com", "테스트 이메일", "안녕하세요! 이것은 테스트 이메일입니다.");
+//            return "이메일이 전송되었습니다.";
+//        } catch (Exception e) {
+//            return "이메일 전송 중 오류가 발생했습니다: " + e.getMessage();
+//        }
+//    }
+
+    @GetMapping("mail")
+    public String test2() throws Exception {
+        emailService.sendSimpleMessageRegist("syhfqq1810@gmail.com");
+        return "완료";
+    }
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
     }
 }
--- backend/ssagri/src/main/java/com/ssafy/ssagri/config/EmailConfig.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/config/EmailConfig.java
@@ -14,50 +14,32 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class EmailConfig {
 
-    @Value("${spring.mail.smtp.port}")
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.port}")
     private int port;
 
-    @Value("${spring.mail.smtp.socketfactory.port}")
-    private int socketPort;
+    @Value("${mail.username}")
+    private String hostEmail;
 
-    @Value("${spring.mail.smtp.auth}")
-    private boolean auth;
+    @Value("${mail.password}")
+    private String hostPassword;
 
-    @Value("${spring.mail.smtp.starttls.enable}")
-    private boolean starttls;
+    @Value("${mail.properties.mail.smtp.auth}")
+    private String auth;
 
-    @Value("${spring.mail.smtp.starttls.required}")
-    private boolean starttls_required;
-
-    @Value("${spring.mail.smtp.socketfactory.fallback}")
-    private boolean fallback;
-
-    @Value("${spring.mail.admin.id}")
-    private String id;
-
-    @Value("${spring.mail.admin.password}")
-    private String password;
-
+    @Value("${mail.properties.mail.smtp.starttls.enable}")
+    private String starttls;
     @Bean
-    public JavaMailSender javaMailService() {
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost("smtp.gmail.com");
-        javaMailSender.setUsername(id);
-        javaMailSender.setPassword(password);
-        javaMailSender.setPort(port);
-        javaMailSender.setJavaMailProperties(getMailProperties());
-        javaMailSender.setDefaultEncoding("UTF-8");
-        return javaMailSender;
-    }
-    private Properties getMailProperties()
-    {
-        Properties pt = new Properties();
-        pt.put("mail.smtp.socketFactory.port", socketPort);
-        pt.put("mail.smtp.auth", auth);
-        pt.put("mail.smtp.starttls.enable", starttls);
-        pt.put("mail.smtp.starttls.required", starttls_required);
-        pt.put("mail.smtp.socketFactory.fallback",fallback);
-        pt.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        return pt;
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(hostEmail);
+        mailSender.setPassword(hostPassword);
+        mailSender.getJavaMailProperties().setProperty("mail.smtp.auth", auth);
+        mailSender.getJavaMailProperties().setProperty("mail.smtp.starttls.enable", starttls);
+        return mailSender;
     }
 }
