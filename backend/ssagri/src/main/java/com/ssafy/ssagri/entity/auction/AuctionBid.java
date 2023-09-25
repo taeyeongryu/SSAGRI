662ac58842e56166dcb88d9e107e6c0491c9cefc
유태영 <tybam@naver.com>
2023-09-25 22:05:46 +0900

    auction bid create

--- backend/ssagri/src/main/java/com/ssafy/ssagri/entity/auction/AuctionBid.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/entity/auction/AuctionBid.java
@@ -1,5 +1,7 @@
 package com.ssafy.ssagri.entity.auction;
 
+import com.ssafy.ssagri.domain.auctionbid.dto.AuctionBidSelectResponseDto;
+import com.ssafy.ssagri.entity.common.BaseTimeEntity;
 import com.ssafy.ssagri.entity.user.User;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -12,9 +14,10 @@ import javax.persistence.FetchType;
 @Getter
 @Table(name = "auction_bid")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AuctionBid {
+public class AuctionBid extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auction_bid_no")
     private Long no;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -24,6 +27,7 @@ public class AuctionBid {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "auction_product_no",nullable = false)
     private AuctionProduct auctionProduct;
+
 
     @Column(name = "auction_bid_price",nullable = false)
     private int price;
@@ -35,5 +39,14 @@ public class AuctionBid {
         this.user = user;
         this.auctionProduct = auctionProduct;
         this.price = price;
+    }
+
+    public AuctionBidSelectResponseDto toResponse(){
+        return AuctionBidSelectResponseDto.builder()
+                .userNo(this.user.getNo())
+                .auctionProductNo(this.auctionProduct.getNo())
+                .auctionBidPrice(this.price)
+                .auctionBidDate(this.getCreateDate())
+                .build();
     }
 }
