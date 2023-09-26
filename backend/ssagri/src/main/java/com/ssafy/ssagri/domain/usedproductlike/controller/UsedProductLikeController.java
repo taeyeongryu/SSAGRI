c6de764d51b14197f57aaf9f809a8b2b4d2c31af
유태영 <tybam@naver.com>
2023-09-26 10:55:51 +0900

    Fix: controller return fix

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/usedproductlike/controller/UsedProductLikeController.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/usedproductlike/controller/UsedProductLikeController.java
@@ -3,6 +3,8 @@ package com.ssafy.ssagri.domain.usedproductlike.controller;
 import com.ssafy.ssagri.domain.usedproduct.service.UsedProductService;
 import com.ssafy.ssagri.domain.usedproductlike.service.UsedProductLikeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,13 +15,15 @@ public class UsedProductLikeController {
     private final UsedProductLikeService usedProductLikeService;
 
     @PostMapping("/{userNo}/{usedProductNo}")
-    public void save(@PathVariable("userNo") final Long userNo, @PathVariable("usedProductNo") final Long usedProductNo) {
+    public ResponseEntity<Void> save(@PathVariable("userNo") final Long userNo, @PathVariable("usedProductNo") final Long usedProductNo) {
         usedProductLikeService.saveUsedProductLike(userNo,usedProductNo);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/{userNo}/{usedProductNo}")
-    public void delete(@PathVariable("userNo") Long userNo,@PathVariable("usedProductNo") Long usedProductNo) {
+    public ResponseEntity<Void> delete(@PathVariable("userNo") Long userNo,@PathVariable("usedProductNo") Long usedProductNo) {
         usedProductLikeService.deleteUsedProductLike(userNo,usedProductNo);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
