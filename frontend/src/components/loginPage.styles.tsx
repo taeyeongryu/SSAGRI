fd2ffc62ee73ba5bfeebbe3ffcce0234d39c742c
황종인 <ghkdwhddls12@daum.net>
2023-10-04 00:39:04 +0900

    save

--- frontend/src/components/loginPage.styles.tsx
+++ frontend/src/components/loginPage.styles.tsx
@@ -290,6 +290,7 @@ const KakaoLogin = styled.div`
 const SignInAndUpComponent = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
+  console.log('state: ', state);
 
   const [image, setImage] = useState(
     'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
