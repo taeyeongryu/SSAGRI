81d856e2f0049fc54589142526481bd4edea87fd
유태영 <tybam@naver.com>
2023-10-05 15:19:22 +0900

    Feat: chat notification

--- backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
+++ backend/ssagri/src/main/java/com/ssafy/ssagri/domain/notification/NotificationService.java
@@ -1,21 +1,21 @@
 package com.ssafy.ssagri.domain.notification;
 
 import com.ssafy.ssagri.domain.auctionbid.repository.AuctionBidRepository;
+import com.ssafy.ssagri.domain.user.repository.UserRegistAndModifyRepository;
 import com.ssafy.ssagri.entity.auction.AuctionBid;
+import com.ssafy.ssagri.entity.user.User;
 import com.ssafy.ssagri.util.exception.CustomException;
 import com.ssafy.ssagri.util.exception.CustomExceptionStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 
@@ -28,7 +28,6 @@ public class NotificationService {
     // send할 때는 경매에 참여하고 있는 사람 select해서 보낸다.
     private final Map<Long, SseEmitter> sseEmitterMap = new ConcurrentHashMap<>();
     private final AuctionBidRepository auctionBidRepository;
-
     public SseEmitter addSseEmitter(Long userNo) {
         log.info("userNo = {}", userNo);
         //60분 짜리 SseEmitter 생성
@@ -52,6 +51,7 @@ public class NotificationService {
         return sseEmitter;
     }
 
+    @Transactional(readOnly = true)
     public void sendMessageToBidder(Long auctionNo, String bidderNickname, int price) {
         log.info("sseEmitterMap.size() = {}", sseEmitterMap.size());
         log.info("sendMessageToBidder");
@@ -91,7 +91,28 @@ public class NotificationService {
             }
         }
     }
+    public void sendMessageToChatter(User sender, User receiver,Long chatRoomNo){
 
+        //만약 receiver가 로그인해서 sseEmitter를 발급 받았다면
+        if(sseEmitterMap.containsKey(receiver.getNo())){
+            SseEmitter sseEmitter = sseEmitterMap.get(receiver.getNo());
+
+            //Json 객체 만든다.
+            String message = new JSONObject()
+                    .put("senderNickname", sender.getNickname())
+                    .put("receiverNickName", receiver.getNickname())
+                    .put("senderNo", sender.getNo())
+                    .put("receiverNo", receiver.getNo())
+                    .put("chatRoomNo", chatRoomNo).toString();
+            try {
+                //메시지 보낸다.
+                sseEmitter.send(SseEmitter.event().name("new chat").data(message));
+            } catch (IOException e) {
+                sseEmitterMap.remove(receiver.getNo());
+                throw new CustomException(CustomExceptionStatus.SSEEMITTER_DOES_NOT_EXIST);
+            }
+        }
+    }
     //Test Method
     public void sendMessageTest(){
         log.info("sseEmitterMap.size() = {}", sseEmitterMap.size());
