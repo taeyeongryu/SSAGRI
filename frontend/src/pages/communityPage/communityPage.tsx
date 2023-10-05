07e154d9560b350a17ddb1c51b4b26fd4d117c6b
joung <15gpfk@naver.com>
2023-10-05 17:47:07 +0900

    Fix: 알림수정

--- frontend/src/pages/communityPage/communityPage.tsx
+++ frontend/src/pages/communityPage/communityPage.tsx
@@ -4,22 +4,22 @@ import Navbar from '../../components/navbar';
 import { LoginPage } from '../../components/loginPage.styles';
 import CommunityMain from '../../components/communityStyle/communityPage.styles';
 import { CommuHeader } from '../../components/header';
-// import { isLoggedInAtom } from '../../states/account/loginAtom';
-// import { useEffect } from 'react';
-// import { useRecoilValue } from 'recoil';
-// import { useNavigate } from 'react-router-dom';
+import { isLoggedInAtom } from '../../states/account/loginAtom';
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { useNavigate } from 'react-router-dom';
 
 const communityPage = () => {
   // 로그인 여부
-  // const isLoggedIn = useRecoilValue(isLoggedInAtom);
-  // const navigate = useNavigate();
+  const isLoggedIn = useRecoilValue(isLoggedInAtom);
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (!isLoggedIn) {
-  //     alert('로그인이 필요한 페이지입니다.');
-  //     navigate('/login');
-  //   }
-  // }, [isLoggedIn]);
+  useEffect(() => {
+    if (!isLoggedIn) {
+      alert('로그인이 필요한 페이지입니다.');
+      navigate('/login');
+    }
+  }, [isLoggedIn]);
   return (
     <div>
       <CommuHeader></CommuHeader>
